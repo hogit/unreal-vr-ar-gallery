b321701ce8c228ca12d361630f3461239cf19739
Rich Hogben <rhogben@supplyframe.com>
2017-08-24 22:18:52 -0700

    Add terrain

--- Build/Android/src/com/Hogben/UnrealVrArGallery/OBBData.java
+++ Build/Android/src/com/Hogben/UnrealVrArGallery/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-90288835L // the length of the file in bytes
+78320941L // the length of the file in bytes
 )
 };
 };
