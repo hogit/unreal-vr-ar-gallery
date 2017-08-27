33b93b7bb155faa4c89b82bceec8ab2229197d1d
Rich Hogben <rhogben@supplyframe.com>
2017-08-27 05:46:41 -0700

    Remove unused assets

--- Build/Android/src/com/Hogben/UnrealVrArGallery/OBBData.java
+++ Build/Android/src/com/Hogben/UnrealVrArGallery/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-80133048L // the length of the file in bytes
+80143495L // the length of the file in bytes
 )
 };
 };
