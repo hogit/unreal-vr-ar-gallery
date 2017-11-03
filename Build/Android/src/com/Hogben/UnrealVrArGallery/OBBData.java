c6983bdfdf363ed3bab25fbf07da1141dad670c8
Rich Hogben <rhogben@supplyframe.com>
2017-11-03 00:20:49 -0700

    Change to dynamic materials

--- Build/Android/src/com/Hogben/UnrealVrArGallery/OBBData.java
+++ Build/Android/src/com/Hogben/UnrealVrArGallery/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-85517952L // the length of the file in bytes
+85559612L // the length of the file in bytes
 )
 };
 };
